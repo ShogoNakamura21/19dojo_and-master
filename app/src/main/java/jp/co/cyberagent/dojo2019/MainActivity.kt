112dfdbae172e426675a63eeb1ed996d62b0aee7
syogo <dmx42ranz@gmail.com>
2019-08-05 14:43:21 +0900

    commit

---
+++ app/src/main/java/jp/co/cyberagent/dojo2019/MainActivity.kt
@@ -0,0 +1,19 @@
+package jp.co.cyberagent.dojo2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+
+
+    }
+}
